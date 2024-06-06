b20a131286120eac9559a050778fb70e0d6463b6
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-06-06 10:22:50 +0530

    auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+        // secure:true,(https only)
         maxAge: age,
       })
       .status(200)
